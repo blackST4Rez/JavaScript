54fb61a990ba6cb58f2807528a072a14afbd57e7
Raka <maharjanraka2015@gmail.com>
2026-01-19 13:22:10 +0545

    numXmaths.js has been added.

---
+++ 01-Basics/numXmaths.js
@@ -0,0 +1,47 @@
+//---------Numbers----------
+
+// const score = 400
+// const balance = new Number(100)
+
+// console.log(score);
+// console.log(balance);
+
+// // toFixed and toString 
+// console.log(balance.toString().length);
+// console.log(typeof balance);
+// console.log(balance.toFixed(2));
+
+
+// // To precision
+// const otherNum = Number("45.45") 
+// console.log(otherNum.toPrecision(3));   
+
+// Locale String
+
+// const num = 1374921384949823;
+// console.log(num.toLocaleString());   
+
+//-----------Maths------------
+
+// Math Methods 
+// let num = -2346.656
+// console.log(Math);
+// console.log(Math.abs(num));
+// console.log(Math.round(num));
+// console.log(Math.ceil(4.5));
+// console.log(Math.floor(2.5));
+
+// Math method ---> Random
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+//Random Math Method with Min and Max
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random() * (max - min + 1)) + min); 
+
+
+
+
+
--- 01-Basics/strings.js
+++ 01-Basics/strings.js
@@ -1,58 +1,58 @@
-const name = "Raka";
-const surName = "Maharjan"; 
+// const name = "Raka";
+// const surName = "Maharjan"; 
 
-console.log(name + surName);
+// console.log(name + surName);
 
-// Template Literals
-console.log(`Hello ! My name is ${name} ${surName}. I am 23 years old.`);
+// // Template Literals
+// console.log(`Hello ! My name is ${name} ${surName}. I am 23 years old.`);
 
-// Prototype and Methods
-const gameName = new String("Football"); // not recommended 
+// // Prototype and Methods
+// const gameName = new String("Football"); // not recommended 
 
-console.log(gameName[0]);
-console.log(gameName.__proto__);
-console.log(gameName.length);
-console.log(gameName.toUpperCase());
-console.log(gameName.toLowerCase());
-console.log(gameName.charAt(4));
-console.log(gameName.indexOf("o"));
-
-
-// Substring and Slice
-const newStr = gameName.substring(0,4);
-const anotherStr = gameName.slice(-8,4);
-
-console.log(newStr);
-console.log(anotherStr);
-
-// Trim
-const newString = "  Raka  ";
-
-console.log(newString);
-console.log(newString.trim());
-
-// Replace
-const replaceStr = gameName.replace("Foot","Basket");
-
-console.log(replaceStr);
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.toLowerCase());
+// console.log(gameName.charAt(4));
+// console.log(gameName.indexOf("o"));
 
 
-// URL Replace
-const url = "https://www.rakamaharjan.com.np/";
+// // Substring and Slice
+// const newStr = gameName.substring(0,4);
+// const anotherStr = gameName.slice(-8,4);
 
-url.replace("https://","");
+// console.log(newStr);
+// console.log(anotherStr);
 
-console.log(url);
-console.log(url.replace("https://","").replace("/",""));
+// // Trim
+// const newString = "  Raka  ";
+
+// console.log(newString);
+// console.log(newString.trim());
+
+// // Replace
+// const replaceStr = gameName.replace("Foot","Basket");
+
+// console.log(replaceStr);
 
 
-// Split
-const splitStr = "apple, banana, mango, orange";
+// // URL Replace
+// const url = "https://www.rakamaharjan.com.np/";
 
-const fruits = splitStr.split("-");
+// url.replace("https://","");
 
-console.log(splitStr);
-console.log(fruits);
+// console.log(url);
+// console.log(url.replace("https://","").replace("/",""));
+
+
+// // Split
+// const splitStr = "apple, banana, mango, orange";
+
+// const fruits = splitStr.split("-");
+
+// console.log(splitStr);
+// console.log(fruits);
 
 
 
