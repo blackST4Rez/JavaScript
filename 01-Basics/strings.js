752d6fa0584366b0e86828cb56181ace01b4f352
Raka <maharjanraka2015@gmail.com>
2026-01-19 12:36:08 +0545

    string.js is completed.

--- 01-Basics/strings.js
+++ 01-Basics/strings.js
@@ -3,8 +3,10 @@ const surName = "Maharjan";
 
 console.log(name + surName);
 
+// Template Literals
 console.log(`Hello ! My name is ${name} ${surName}. I am 23 years old.`);
 
+// Prototype and Methods
 const gameName = new String("Football"); // not recommended 
 
 console.log(gameName[0]);
@@ -15,21 +17,44 @@ console.log(gameName.toLowerCase());
 console.log(gameName.charAt(4));
 console.log(gameName.indexOf("o"));
 
+
+// Substring and Slice
 const newStr = gameName.substring(0,4);
-
-console.log(newStr);
-
 const anotherStr = gameName.slice(-8,4);
 
+console.log(newStr);
 console.log(anotherStr);
 
+// Trim
 const newString = "  Raka  ";
 
 console.log(newString);
 console.log(newString.trim());
 
+// Replace
 const replaceStr = gameName.replace("Foot","Basket");
 
 console.log(replaceStr);
+
+
+// URL Replace
+const url = "https://www.rakamaharjan.com.np/";
+
+url.replace("https://","");
+
+console.log(url);
+console.log(url.replace("https://","").replace("/",""));
+
+
+// Split
+const splitStr = "apple, banana, mango, orange";
+
+const fruits = splitStr.split("-");
+
+console.log(splitStr);
+console.log(fruits);
+
+
+
 
 
