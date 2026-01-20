3b5b350da4831a331443bbd3b240c307b5eac835
Raka <maharjanraka2015@gmail.com>
2026-01-20 16:08:08 +0545

    functions.js has been added.

---
+++ 03-Basics/functions.js
@@ -0,0 +1,44 @@
+// Basics function syntax
+// function sayMyName () {
+//     console.log('Bruno');
+//     console.log('Bruno');
+//     console.log('Bruno');
+//     console.log('Bruno');
+//     console.log('Bruno');
+// }
+// sayMyName();
+
+// Function with parameters and arguments
+// function addTwoNumbers(a,b){
+//     console.log(a + b); 
+// }
+// const result = addTwoNumbers(3,5); // function call can be stored in a variable
+// console.log(result); // a mistake
+
+// Way to store value in a variable with the use of return
+// function addTwoNumbers(num1,num2){
+//     let res = num1+ num2;
+//     return res;
+// }
+// const res = addTwoNumbers(3,5);
+// console.log(`Result is ${res}.`);
+
+// funtion with if satatements
+//----> here in paramete if we give sam it is a default value meaning the function is never undefined
+//----> if sam was not given in parameter then if statement is executed 
+//---->without if statement and default parameter value Raka is printed cause it is passed in the arguments
+
+// function loginUserMessage(username = 'Sam'){
+//     if(!username){
+//         console.log(`Please enter your name:`);
+//         return;
+//     }
+//     return `Welcome back, ${username}!`;      
+// }
+// const message = loginUserMessage('Raka');     
+// console.log(message);
+
+
+
+
+
