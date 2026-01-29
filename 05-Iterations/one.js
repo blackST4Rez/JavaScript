b5b63afe9f601b95975837c17e8a13f4d09cdecf
Raka <maharjanraka2015@gmail.com>
2026-01-29 14:43:15 +0545

    Loops has been added.

---
+++ 05-Iterations/one.js
@@ -0,0 +1,34 @@
+// For loop
+
+// for (let i = 0; i <= 10; i++) {
+//     const value = i;
+//     if (i==5) {
+//         console.log(`5 is the best number.`); 
+//     }
+//         console.log(value);
+// }
+
+// Nested loop and here child loop with index j  can use index i of parent loop
+
+// for (let i = 0; i <=10 ; i++) {
+//     console.log(`Outer Loop value : ${i}`);
+    
+//     for (let j = 0; j <=10 ; j++) {
+//         // console.log(`Inner Loop value : ${j} and Parent value : ${i}`);
+//         // console.log(i + '*' + j + '=' + i*j);
+//         console.log(`${i}*${j} = ${i*j}`);
+//     }    
+// }
+
+// Break and Continue 
+
+// for (let i = 0; i <=20 ; i++) {
+//     if (i==5) {
+//         console.log(`Detected 5!`);
+//         continue
+//     }
+//     console.log(`Value of i is ${i}`);
+// }
+
+
+
---
+++ 05-Iterations/two.js
@@ -0,0 +1,26 @@
+// While Loop
+
+// let i = 0;
+// while (i<=10) {
+//     console.log(`Value of i is ${i}`);
+//     i = i + 2;
+// }
+
+// let arr = ['Batman', 'Superman', 'Spiderman']
+// let ir = 0
+
+// console.log(arr.length);
+
+// while (ir < arr.length) {
+//     console.log(`Heroe is ${arr[ir]}`);    
+//     ir++;
+// }
+
+// Do While Loop
+
+let score = 1
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
