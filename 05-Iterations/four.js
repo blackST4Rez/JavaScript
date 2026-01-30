0d1aa2145ce3a065f627d36a9753a55997147625
Raka <maharjanraka2015@gmail.com>
2026-01-30 15:22:53 +0545

    Iterations has been added.

---
+++ 05-Iterations/four.js
@@ -0,0 +1,18 @@
+// const obj = {
+//     js: "javascript",
+//     cpp: "C++",
+//     rb: "uby",
+//     swift: "swift by apple"
+// }
+
+// for (const key in obj) {
+//     console.log(`${key} is shortcut for ${obj[key]}`);
+    
+// }
+
+const lang = ['rb', 'js', 'py', 'java']
+
+for (const key in lang) {
+    console.log(lang[key]);
+    
+}
---
+++ 05-Iterations/three.js
@@ -0,0 +1,34 @@
+// For of 
+
+// const arr = [1, 2, 3, 4, 5]
+
+// for (const num of arr) {
+//     console.log(num);
+// }
+
+// const greetings = "Hello World !"
+// for (const greet of greetings) {
+//     console.log(`Each char is ${greet}`);
+    
+// }
+
+// Maps 
+// const map = new Map()
+// map.set('NP',"Nepal")
+// map.set('AUS',"Australia")
+// map.set('JP',"Japan")
+
+// console.log(map);
+
+// for (const [key, value] of map) {
+//     console.log(key, value);   
+// }
+
+// Mapping in the object
+// const obj1 = {
+//     'game1': 'NFS',
+//     'game2': 'Valorant'
+// }
+// for (const [key, value] of obj1) {
+//     console.log(key, value); 
+// }
