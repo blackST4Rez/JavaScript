cf10c4bf847747f68dcd28123a5e131fb46110d5
blackST4Rez <maharjanraka2015@gmail.com>
2026-07-17 13:35:34 +0545

    variable.js was updated.

--- 01-Basics/variable.js
+++ 01-Basics/variable.js
@@ -1,23 +1,25 @@
-const accountID = 12345
-let accountEmail = "raka2025@gmail.com"
+// Keywords like const, let, var and let are pre-defined in the javascipt compiler.
+
+const accountId = 983424
+let accountEmail = "raka2026@gmail.com"
 var accountPassword = "12345"
 let accountState;
 accountCity = "Kathmandu"
 
-// accountID = 2
+// accountId = 12345 after const the value cannot be changed // not allowed
 
 accountEmail = "hari2025@gmail.com"
 accountPassword = "324345"
-accountCity = "Chitwan"
+accountCity = "Pokhara"
 
-/*
-Use const and let more
-Prefer not use var 
-Cause of issue in block scope and functional scope
+//Use const and let more, Prefer not use var Cause of issue in block scope and functional scope
 
-when variable is decalred with no value it is undefined 
-*/
+// when variable is decalred with no value it is undefined
+//
 
-console.log(accountID);
-console.table([accountID, accountEmail, accountPassword, accountCity, accountState]);
+// console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+// .table() is a method to print the data in a tabular structure. 
 
