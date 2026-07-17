b43fd6a561f95ffd86ca915d538fcbe643432263
blackST4Rez <maharjanraka2015@gmail.com>
2026-07-17 13:53:24 +0545

    datatypes.js was updated.

--- 01-Basics/datatypes.js
+++ 01-Basics/datatypes.js
@@ -1,36 +1,43 @@
 // "use strict"; //treat all JS code as newer version
 
-// alert(3+3) // we are using nodejs not browser
+// alert(3+3) we are using nodejs not browser
 
-// console.log(3
-//     +
-//     3); 
+/*console.log(3
+    +
+    3); 
+*/
 
-// console.log // code readability should be high 
-// ("Raka" // avoid such way of writing code
+// console.log(Raka); code readability should be high
 
-// );
+/*
+console.log(
+"Raka")
+; 
 
+avoid such way of writing code
+*/
 
-// let name = "Raka"
-// let age = 23 
-// let isLoggedIn = true // boolean can be true or false only
+/*
+let name = "Raka"
+let age = 23 
+let isLoggedIn = true // boolean can be true or false only
+*/
 
 // number --> 2 to power 53
-// bigint 
+// bigint
 // string --> ""
 // boolean --> true/false
 // null --> standalone value ( symbolizes the nothingness )
-// undefined --> value is not assigned but the variable is defined 
+// undefined --> value is not assigned but the variable is defined
 // symbol --> unique
 
+/*
 //object
-
-// console.log(typeof undefined); // undefined is the answer
-// console.log(typeof null); // object is the answer
+console.log(typeof undefined); // undefined is the answer
+console.log(typeof null); // object is the answer
+*/
 
 /*
-
 // Primitive(Non-Referenced) & Non-Primitive(Referenced)
 
 ----Non-Referenced----
