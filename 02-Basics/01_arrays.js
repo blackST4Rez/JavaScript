97187dc90de27ec8c584b7b2edebb839a6fee481
blackST4Rez <maharjanraka2015@gmail.com>
2026-07-18 14:33:38 +0545

    Array and array methods was updated.

--- 02-Basics/01_arrays.js
+++ 02-Basics/01_arrays.js
@@ -1,41 +1,47 @@
-// Arrays
+/*
+--Arrays
 
 const myArr = [0, 1, 2, 3, 4, 5];
 const myHeroes = ["Superman", "Batman", "Flash", "Spiderman"];      
 const myArr2 = new Array(1,2,3,4,5); 
 
-// console.log(myArr[2]);
+console.log(myArr[2]); --shows the letter at index 2
+*/
 
-// Array Methods
+/*
+--Array Methods
 
-// myArr.push(6);
-// myArr.push(7);
-// myArr.pop();
+myArr.push(6);
+myArr.push(7);
+myArr.pop(); -- removes the value always from the last index
+console.log(myArr);
+*/
 
-// myArr.unshift(9);  
-// myArr.shift(); 
+/*
+myArr.unshift(9); --pushes the value at the start of the index which is not good for large array values
+myArr.shift()
 
-// console.log(myArr.includes(89));
-// console.log(myArr.includes(5));
-// console.log(myArr.indexOf(89));
-// console.log(myArr.indexOf(3));
-// console.log(myArr);
+console.log(myArr.includes(89)); -- answers in boolean if the value exist in the array or not
+console.log(myArr.includes(5));
+console.log(myArr.indexOf(89)); -- answers the index of the given value
+console.log(myArr.indexOf(3));
+*/
 
-// const newArr = myArr.join();
+/*
+const newArr = myArr.join(); -- changes the array into string
+console.log(newArr);
+console.log(typeof newArr);
 
-// console.log(myArr);
-// console.log(typeof newArr);
-
-// Slice vs Splice
+-- Slice vs Splice
 console.log( "A", myArr);
-
 const myn = myArr.slice(1,4);
 console.log(myArr);
-console.log( "B", myn);
 
+console.log( "B", myn);
 const myn2 = myArr.splice(1,4);
 console.log(myArr);
 console.log( "C", myn2);
+*/
 
 
 
