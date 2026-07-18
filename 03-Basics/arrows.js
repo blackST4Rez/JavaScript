e8f4b3f146e26c6cd89b9da9d786ddb593455ff2
blackST4Rez <maharjanraka2015@gmail.com>
2026-07-18 18:40:08 +0545

    this keyword, arrow function, implicit return and object as parameter has been updated.

--- 03-Basics/arrows.js
+++ 03-Basics/arrows.js
@@ -1,51 +1,55 @@
-// const user = {
-//     username: 'Raka Maharjan',
-//     price: 199,
-    
-//     welcomeMessage: function(){
-//         console.log(`${this.username}, Welcome Back!`);
-//         console.log(this);
-        
-//     }
-// };  
+/*
+const user = {
+    username: 'Raka Maharjan',
+    price: 199,
+    welcomeMessage: function(){
+        console.log(`${this.username}, Welcome Back!`); // (this) keyword targets the current context.
+        console.log(this)     
+    }
+};  
 
-// user.welcomeMessage(); // Context is not changed here
-// user.username = 'Sam'; // Context changes here
-// user.welcomeMessage();
+user.welcomeMessage(); // Context is not changed here
+user.username = 'Sam'; // Context changes here
+user.welcomeMessage(); //Output of the changed context
+*/
 
 // console.log(this);
 
 // function abc() {
-//     let username = 'Raka';      
+//     let username = 'Raka';
 //     console.log(this.username);
 // }
 // abc();
 
 // const xyz = function cde() {
-//     let username = 'Raka';      
+//     let username = 'Raka';
 //     console.log(this.username);
 // }
 
 // const xyz = () => {
-//     let username = 'Raka';      
+//     let username = 'Raka';
 //     console.log(this);
 // };
 // xyz();
 
+/*
 // Arrow Function
-//------Basic Arrow Function Syntax------       
-// const addTwoNumbers = (num1, num2) => {
-//     return num1 + num2;     
-// }
-// console.log(addTwoNumbers(9, 5));
+// ------Basic Arrow Function Syntax------  
+// when using curly brackets you ALWAYS use return     
+const addNumbers = (num1, num2) => {  
+    return num1 + num2;     
+}
+console.log(addNumbers(9, 5));
 
 //------Implicit Return------
-// const addTwoNumbers = (num1, num2) => num1 + num2;
-// console.log(addTwoNumbers(9, 5));
+// when using small brackets you dont use return
+const addTwoNumbers = (num1, num2) => (num1 + num2); 
+console.log(addTwoNumbers(9, 5));
 
 //------Object Parameter------
 // Object needs to be wrapped in parenthesis to avoid syntax error      
-// const addTwo = (num1, num2) => ({username: 'Raka Maharjan'});
-// console.log(addTwo(3, 5));
+const addTwo = (username, plan) => ({username: 'Raka Maharjan', plan: 'Premium'});
+console.log(addTwo());
+*/
 
 
