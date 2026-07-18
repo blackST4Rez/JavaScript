0548f1eabdeb38a80c365565b0899ef84eb96fd5
blackST4Rez <maharjanraka2015@gmail.com>
2026-07-18 14:50:44 +0545

    Array concat and spread with some more array method was updated.

--- 02-Basics/02_arrays.js
+++ 02-Basics/02_arrays.js
@@ -3,24 +3,24 @@ const dcHeroes = ["Batman", "Superman", "Flash", "Wonderwoman"];
 
 // marvelHeroes.push(dcHeroes);
 // console.log(marvelHeroes); 
-// console.log(marvelHeroes[3][1]);              
+// console.log(marvelHeroes[4][1]);              
 
-// const allHeroes = marvelHeroes.concat(dcHeroes); 
+// const allHeroes = marvelHeroes.concat(dcHeroes); -- adds both arrays and shows as a one array
 // console.log(allHeroes);
 
-const allnewHeroes = [...marvelHeroes, ...dcHeroes];
-console.log(allnewHeroes);
+// const allnewHeroes = [...marvelHeroes, ...dcHeroes]; -- spread operator 
+// console.log(allnewHeroes);
 
-const anotherArr = [1,2,3,[4,5,6],7,8,9,[3,4,[5,7],7,9,0]];
-console.log(anotherArr.flat(Infinity));
+// const anotherArr = [1,2,3,[4,5,6],7,8,9,[3,4,[5,7],7,9,0]];
+// console.log(anotherArr.flat(Infinity)); -- just like spreading but has the depth of the array
 
-console.log(Array.isArray("RakaMaharjan"));
-console.log(Array.from("RakaMaharjan"));
-console.log(Array.from({name : "Raka"}));
+// console.log(Array.isArray("RakaMaharjan"));
+// console.log(Array.from("RakaMaharjan"));
+// console.log(Array.from({name : "Raka"}));
 
-let score1 = 100;
-let score2 = 200;
-let score3 = 300;       
+// let score1 = 100;
+// let score2 = 200;
+// let score3 = 300;       
 
-console.log(Array.of(score1,score2,score3));
+// console.log(Array.of(score1,score2,score3));
 
