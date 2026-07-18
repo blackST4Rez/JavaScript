c831268ddd36c50fa10e4d7d4e8a708f3eac4298
blackST4Rez <maharjanraka2015@gmail.com>
2026-07-18 16:49:53 +0545

    Basic funtion was updated.

--- 03-Basics/functions.js
+++ 03-Basics/functions.js
@@ -1,30 +1,38 @@
-// Basics function syntax
-// function sayMyName () {
-//     console.log('Bruno');
-//     console.log('Bruno');
-//     console.log('Bruno');
-//     console.log('Bruno');
-//     console.log('Bruno');
-// }
-// sayMyName();
+/*
+Basics function syntax
+function sayMyName () {
+    console.log('Bruno');
+    console.log('Bruno');
+    console.log('Bruno');
+    console.log('Bruno');
+    console.log('Bruno');
+}
+sayMyName; -- reference of the function
+sayMyName(); --execution of the function
+*/
 
-// Function with parameters and arguments
-// function addTwoNumbers(a,b){
-//     console.log(a + b); 
-// }
-// const result = addTwoNumbers(3,5); // function call can be stored in a variable
-// console.log(result); // a mistake
+/*
+Function with parameters and arguments
 
-// Way to store value in a variable with the use of return
-// function addTwoNumbers(num1,num2){
-//     let res = num1+ num2;
-//     return res;
-// }
-// const res = addTwoNumbers(3,5);
-// console.log(`Result is ${res}.`);
+function addTwoNumbers(a,b){
+    console.log(a + b); 
+}
+ const result = addTwoNumbers(3,5); // function call can be stored in a variable
+ console.log(result); // a mistake
+ */
+
+/*
+Way to store value in a variable with the use of return
+function addTwoNumbers(num1,num2){
+    let res = num1+ num2;
+    return res;
+}
+const res = addTwoNumbers(3,5);
+console.log(`Result is ${res}.`);
+*/
 
 // function with if statements
-//----> here in paramete if we give sam it is a default value meaning the function is never undefined
+//----> here in parameter if we give sam it is a default value meaning the function is never undefined
 //----> if sam was not given in parameter then if statement is executed 
 //---->without if statement and default parameter value Raka is printed cause it is passed in the arguments
 
