5698996d0b4fe0c4f7ec89e8e8243a406ac754da
blackST4Rez <maharjanraka2015@gmail.com>
2026-07-19 13:55:58 +0545

    Truthy and Falsy value, Nullish Coalescing Operator and Terniary operator was updated.

--- 04-ControlFlow/truthy.js
+++ 04-ControlFlow/truthy.js
@@ -1,42 +1,40 @@
-// Truthy Javascript
+// -- Truthy Javascript
 const userEmail = []
 
-if (userEmail) {
-    console.log("Got user email");
-} else {
-    console.log("Don't have user email");
-}
+// if (userEmail) {
+//     console.log("Got user email");
+// } else {
+//     console.log("Don't have user email");
+// }
 
-// falsy values
+// -- Falsy values
 
 // false, 0, -0, BigInt 0n, "", null, undefined, NaN
 
-//truthy values
-// "0", 'false', " ", [], {}, function(){}
+//-- Truthy values
+// "0", 'false', " ", [], {}, function(){} --> is an empty function which is also a truthy value.
 
-// if (userEmail.length === 0) {
+// if (userEmail.length === 0){
 //     console.log("Array is empty");
 // }
 
-const emptyObj = {}
+// const emptyObj = {}
 
-if (Object.keys(emptyObj).length === 0) {
-    console.log("Object is empty");
-}
+// if (Object.keys(emptyObj).length === 0) {
+//     console.log("Object is empty");
+// }
 
-// Nullish Coalescing Operator (??): null undefined
+// -- Nullish Coalescing Operator (??): null undefined
 
 let val1;
 // val1 = 5 ?? 10
 // val1 = null ?? 10
 // val1 = undefined ?? 15
-val1 = null ?? 10 ?? 20
-
-
+val1 = null ?? 10 ?? 20 -- returns 10 because right after getting 10 it does not take 20
 
 console.log(val1);
 
-// Terniary Operator
+// -- Terniary Operator
 
 // condition ? true : false
 
