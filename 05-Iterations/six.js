f7cc5fbf4b0dba625402b0840e92081c30ba998b
blackST4Rez <maharjanraka2015@gmail.com>
2026-07-21 22:52:28 +0545

    For each and Filter has been updated.

--- 05-Iterations/six.js
+++ 05-Iterations/six.js
@@ -1,3 +1,4 @@
+// -- For Each
 // const coding = ["js", "ruby", "java", "python", "cpp"]
 
 
@@ -8,7 +9,7 @@
 
 // console.log(values);
 
-// Filter
+// -- Filter
 const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 // const newNums = myNums.filter( (num) => {
