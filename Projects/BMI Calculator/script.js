59346a1ec3be79a0433d14cbbce253ada42613f4
blackST4Rez <maharjanraka2015@gmail.com>
2026-07-23 01:17:07 +0545

    simple js for the bmi of the body was updated.

--- Projects/BMI Calculator/script.js
+++ Projects/BMI Calculator/script.js
@@ -1,19 +1,45 @@
-const form = document.querySelector('form')
+// Simple JS for calculating BMI of the body.
 
-form.addEventListener('submit',function(e){
-    e.preventDefault()
+const form = document.querySelector('form');
+// this will store an empty value in hg if targeted outside an event listener.
+// const hg = parseInt(document.querySelector('#height').value);
 
-    const height = parseInt(document.querySelector('#height').value)
-    const weight = parseInt(document.querySelector('#weight').value)
-    const results = document.querySelector('#results')
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
 
-    if (height === '' || height < 0 || isNaN(height)) {
-        results.innerHTML = 'Please provide a valid height.'
-    }else if
-    (weight === '' || weight < 0 || isNaN(weight)) {
-        results.innerHTML = 'Please provide a valid weight.'
-    }else{
-        const bmi = (weight / ((height * height)/10000)).toFixed(2);
-        results.innerHTML = `<span>${bmi}</span>`;
+    const hg = parseInt(document.querySelector('#height').value);
+    const wg = parseInt(document.querySelector('#weight').value);
+    const res = document.querySelector('#results');
+    const bmi = (wg / ((hg / 100) ** 2)).toFixed(2);
+
+    if (hg === '' || hg < 0 || isNaN(hg)) {
+        res.innerHTML = 'Please enter a valid height.';
     }
-})
+
+    else if (wg === '' || wg < 0 || isNaN(wg)) {
+        res.innerHTML = 'Please enter a valid weight.';
+    }
+        
+    else {
+        res.innerHTML = `The BMI of your body is ${bmi}.`
+    }
+
+
+    let guide = '';
+
+    if (bmi < 18.6) {
+        guide = 'Under Weight';
+    }
+    
+    else if (bmi >= 18.6 && bmi <= 24.9) {
+        guide = 'Normal Weight';
+    }
+    
+    else {
+        guide = 'Over Weight';
+    }
+
+    res.innerHTML = `
+    Your BMI is <strong>${bmi}</strong>.<br><br>
+    Weight Guide: <strong>${guide}</strong>`;
+});
