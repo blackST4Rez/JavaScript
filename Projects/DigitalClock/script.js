7be3585cf8624cf0a71afcc03140bef5c6a719df
blackST4Rez <maharjanraka2015@gmail.com>
2026-07-23 20:21:42 +0545

    Simple JS for the default clock was updated.

--- Projects/DigitalClock/script.js
+++ Projects/DigitalClock/script.js
@@ -1,7 +1,13 @@
+// Simple JS for a default clock.
+
 const clock = document.getElementById('clock');
 
-setInterval(function () {
-    let date = new Date ();
-        clock.innerHTML = date.toLocaleTimeString();
-    }, 1000)
+// setInterval(function () {
+//     let date = new Date ();
+//         clock.innerHTML = date.toLocaleTimeString();
+//     }, 1000)
 
+setInterval(function () {
+    let date = new Date();
+    clock.innerHTML = date.toLocaleTimeString();
+},1000)
