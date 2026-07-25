2dc8bac71a84d0bce13a760c9826e19de08676f6
blackST4Rez <maharjanraka2015@gmail.com>
2026-07-25 14:04:28 +0545

    Bubbling and Capturing in an event.

--- 06-DOM/Bubble&Capture/script.js
+++ 06-DOM/Bubble&Capture/script.js
@@ -1,6 +1,31 @@
 let ul = document.querySelector('ul')
     
-ul.addEventListener('click', function (evt) {
-    console.log(evt.target)
-    evt.target.classList.toggle('lt')
-})
+// ul.addEventListener('click', function (evt) {
+//     console.log(evt.target)
+//     evt.target.classList.toggle('lt')
+// })
+
+let a = document.querySelector('.a')
+let b = document.querySelector('.b')
+let c = document.querySelector('.c')
+let btn = document.querySelector('button')
+
+btn.addEventListener('click',function (evt) {
+    console.log('button clicked');
+    
+},)
+
+c.addEventListener('click',function (evt) {
+    console.log('c clicked');
+    
+},)
+
+b.addEventListener('click',function (evt) {
+    console.log('b clicked');
+    
+},)
+
+a.addEventListener('click',function (evt) {
+    console.log('a clicked');
+    
+},true)
